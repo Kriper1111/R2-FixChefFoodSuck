3e96386569d1e7e1f62b253bf9e816eecab029a0
Kriper1111 <31844440+Kriper1111@users.noreply.github.com>
2024-09-05 01:10:18 +0300

    AcridYump: More thorough damage potential check
    That way you can fall at a high speed into a wall or an enemy and still
    explode on contact. Still broken for guest in coop. Have ideas.

--- AcridYump/BaseLeapMixin.cs
+++ AcridYump/BaseLeapMixin.cs
@@ -6,23 +6,45 @@ namespace AcridYump;
 
 public class BaseLeapMixin {
     private readonly BaseLeap _self;
-    public bool DetonateNextNextFrame;
+    private bool _isDangerous;
 
     public BaseLeapMixin(BaseLeap other) {
         _self = other;
+        _isDangerous = false;
     }
     
     public void OnMovementHit(ref CharacterMotor.MovementHitInfo info) {
-        if (_self.isAuthority) {
-            var isDangerous = IsFallDamageDangerous(_self.characterBody, info);
-            if (isDangerous) return;
+        if (_self.isAuthority && !_isDangerous) {
+            // But yea, we just.. Don't explode or clear fall immunity flag
+            // if we can guess that this collision _may_ deal damage.
+            _isDangerous |= IsFallDamageDangerous(_self.characterBody, info);
+            // This makes it not as snappy in multiplayer context
+            // But even then, we do risk triggering OnGroundHitServer on the dangerous hit,
+            // and get the danger flag cleared the next few frames.
+            // I'd put reliability over responsiveness, and this only can happen in guest multiplayer.
+            if (_isDangerous) return;
             _self.detonateNextFrame = true;
         }
     }
     
     private static bool IsFallDamageDangerous(CharacterBody body, CharacterMotor.MovementHitInfo info) {
+        // Partially gutted from GlobalEventManager.OnHitGroundServer
+        // and loosely interpreted from KinematicCharacterMotor.ProbeGround
+        // Yes it's _That_ bad
         if (!(info.velocity.y < 0)) return false;
         var velocity = Mathf.Abs(info.velocity.y);
-        return Mathf.Max(velocity - (body.jumpPower + 20), 0.0f) > 0.0f;
+        var canDealDamage = Mathf.Max(velocity - (body.jumpPower + 20), 0.0f) > 0.0f;
+
+        if (!canDealDamage) return false;
+        
+        var motor = body.characterMotor.Motor;
+        var maxMovement = info.velocity.magnitude;
+        
+        if (info.hitCollider.Raycast(new Ray(body.footPosition, Vector3.down), out var raycastHit, maxMovement)) {
+            return (Vector3.Angle(motor._characterUp, raycastHit.normal) <= motor.MaxStableSlopeAngle);
+        }
+
+        // we assume it's still a dangerous hit, even if there isn't anything in that direction
+        return true;
     }
 }
